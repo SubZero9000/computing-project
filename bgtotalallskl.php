81679ebe413e312a6a09236c86f4049e7f8cd0f2
Kabir Roshan <kabir.roshan@hotmail.co.uk>
2016-03-09 23:21:07 +0000

    New changes made
    - resultEntryInputForm.php now has 3 columns complete (name, textbox, drop-down lists), all unique to each name.
    - Indentation added too these 3 files making it easier to read.

--- bgtotalallskl.php
+++ bgtotalallskl.php
@@ -3,59 +3,59 @@
 <?php
 // Connecting to SQL server
 $servername = "localhost";
-$username = "root";
-$password = "Lampserve1";
-$dbname = "brent_athletics";
+$username   = "root";
+$password   = "Lampserve1";
+$dbname     = "brent_athletics";
 
 // Creates connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 } else {
-	echo "Connected!";
+    echo "Connected!";
 }
 echo "<br>";
 
 // FUNCTION which calculates the sum of the boys and girls for each school 
 
-function bgTotSkl($bgTotSkl_SchoolName, $conn) {
-// This if statement prevents SQL injection
-	if(isset($bgTotSkl_SchoolName)){
- 		$bgTotSkl_SchoolName = mysqli_real_escape_string($conn,$bgTotSkl_SchoolName);
-} 
-// Query called $bgTotSkl_Query, uses php variable called $bgTotSkl_SchoolName inorder 
-// to provide an input to specify a specific school
-	$bgTotSkl_Query = "SELECT SUM(result_studpoints) AS totalbg, stud_gender
-	FROM result
-	JOIN students ON result.stud_id WHERE result.stud_id = students.stud_id
-	AND stud_school = '$bgTotSkl_SchoolName' 
-	GROUP BY stud_gender";
-	$mainQuery = mysqli_query($conn, $bgTotSkl_Query);
-
-// Above $mainQuery executes query and stores the results as a table called $mainQuery  
-// $data stores the results of the query for each line within while loop
-while ($data = mysqli_fetch_assoc($mainQuery)) {
-echo "Total $bgTotSkl_SchoolName " . $data['stud_gender'] . ": " . $data['totalbg'] . "<br>"; 
-	}
+function bgTotSkl($bgTotSkl_SchoolName, $conn)
+{
+    // This if statement prevents SQL injection
+    if (isset($bgTotSkl_SchoolName)) {
+        $bgTotSkl_SchoolName = mysqli_real_escape_string($conn, $bgTotSkl_SchoolName);
+    }
+    // Query called $bgTotSkl_Query, uses php variable called $bgTotSkl_SchoolName inorder 
+    // to provide an input to specify a specific school
+    $bgTotSkl_Query = "SELECT SUM(result_studpoints) AS totalbg, stud_gender
+    FROM result
+    JOIN students ON result.stud_id WHERE result.stud_id = students.stud_id
+    AND stud_school = '$bgTotSkl_SchoolName' 
+    GROUP BY stud_gender";
+    $mainQuery      = mysqli_query($conn, $bgTotSkl_Query);
+    
+    // Above $mainQuery executes query and stores the results as a table called $mainQuery  
+    // $data stores the results of the query for each line within while loop
+    while ($data = mysqli_fetch_assoc($mainQuery)) {
+        echo "Total $bgTotSkl_SchoolName " . $data['stud_gender'] . ": " . $data['totalbg'] . "<br>";
+    }
 }
 
 // MySQL Query gets the distinct Schools in database
 // The distinct schools are then stored in an array called $allSchools 
 $sllSchoolsQuery = "SELECT DISTINCT stud_school AS schools FROM students";
-$schoolNames = mysqli_query($conn, $sllSchoolsQuery);
+$schoolNames     = mysqli_query($conn, $sllSchoolsQuery);
 while ($row = mysqli_fetch_assoc($schoolNames)) {
-	$arraySkl[] = $row['schools'];
+    $arraySkl[] = $row['schools'];
 }
-var_dump($arraySkl); 
+var_dump($arraySkl);
 echo "<br>";
 
 // This will loop through the associative array called $arraySkl
 // Executing function
-foreach($arraySkl as $value) {
-bgTotSkl($value, $conn);
+foreach ($arraySkl as $value) {
+    bgTotSkl($value, $conn);
 }
 mysqli_close($conn);
 ?>
 </html>
-
--- resultEntryInputForm.php
+++ resultEntryInputForm.php
@@ -1,29 +1,31 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <?php
 // Connecting to SQL server
 $servername = "localhost";
-$username = "root";
-$password = "Lampserve1";
-$dbname = "brent_athletics";
+$username   = "root";
+$password   = "Lampserve1";
+$dbname     = "brent_athletics";
 
 // Creates connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
-die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 } else {
-	echo "Connected!";
+    echo "Connected!";
 }
 echo "<br>";
 
-$event = $_GET['cmbEvent'];
-$yrGroup  = $_GET['cmbYrGroup'];
+// These gets the values entered in the drop-down lists and stores the values as
+// variables.
+$event   = $_GET['cmbEvent'];
+$yrGroup = $_GET['cmbYrGroup'];
 $gender  = $_GET['cmbGender'];
 
 /* The query $eygQuery, where the 'eyg' stands for event, year group and gender, 
 grabs the students who meet the requirements of the specific stud_gender, 
-stud_yrgroup and event_id where a subquery is used to get the event_id. */  
+stud_yrgroup and event_id where a subquery is used to get the event_id. */
 $eygQuery = "
   SELECT stud_fname, stud_sname
   FROM students
@@ -35,17 +37,26 @@ $result = mysqli_query($conn, $eygQuery);
 
 //Checks to see if $result query works
 if (!$result) {
-echo "FAIL";
+    echo "FAIL";
 }
 
 // Output
 while ($data = mysqli_fetch_assoc($result)) {
-echo $data['stud_fname'] . " " . $data['stud_sname'] . "<br />"; 
-$arrayName[] = $data['stud_fname'] ." ". $data['stud_sname'];
+    //echo $data['stud_fname'] . " " . $data['stud_sname'] . "<br />"; 
+    $arrayName[] = $data['stud_fname'] . " " . $data['stud_sname'];
 }
 
-var_dump($arrayName); 
-echo "<br>";
+// outputs the text boxes for each name in the array.
+foreach ($arrayName as $value) {
+    echo $value;
+    $value = str_replace(' ', '', $value);
+    $value .= 'Result';
+    echo "<input type='text' name='$value' value='$value' />";
+    $value = str_replace('Result', '', $value);
+    $value .= 'Position';
+    echo "<select name='$value'><option value='$value'>$value</option></select>";
+    echo "<br>";
+}
 
 mysqli_close($conn);
 ?>
