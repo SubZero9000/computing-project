11e5cd03362fbcc37dec50fafc4112066c17a27e
Kabir Roshan <kabir.roshan@hotmail.co.uk>
2016-03-18 14:49:31 +0000

    Updates

--- resultEntryInputForm.php
+++ resultEntryInputForm.php
@@ -27,11 +27,11 @@ $gender  = $_GET['cmbGender'];
 grabs the students who meet the requirements of the specific stud_gender, 
 stud_yrgroup and event_id where a subquery is used to get the event_id. */
 $eygQuery = "
-  SELECT stud_fname, stud_sname
+  SELECT stud_fname, stud_sname, stud_id
   FROM students
   WHERE stud_gender = '$gender'
   AND stud_yrgroup = '$yrGroup'
-  AND stud_id IN (SELECT stud_id FROM result WHERE event_id = '$event') 
+  AND stud_id IN (SELECT stud_id FROM result WHERE event_id = '$event')  
 ";
 $result = mysqli_query($conn, $eygQuery);
 
@@ -43,10 +43,13 @@ if (!$result) {
 // Output
 while ($data = mysqli_fetch_assoc($result)) {
     //echo $data['stud_fname'] . " " . $data['stud_sname'] . "<br />"; 
-    $arrayName[] = $data['stud_fname'] . " " . $data['stud_sname'];
+    $arrayName[] = $data['stud_id']." ".$data['stud_fname'] . " " . $data['stud_sname'];
 }
 
-// outputs the text boxes for each name in the array.
+// A function that count the number of rows in the query $result
+$noStudents = mysqli_num_rows($result);
+
+// outputs the text boxes and drop-down lists for each name in the array.
 foreach ($arrayName as $value) {
     echo $value;
     $value = str_replace(' ', '', $value);
@@ -54,9 +57,20 @@ foreach ($arrayName as $value) {
     echo "<input type='text' name='$value' value='$value' />";
     $value = str_replace('Result', '', $value);
     $value .= 'Position';
-    echo "<select name='$value'><option value='$value'>$value</option></select>";
+    echo "<select name='$value'>";
+       for ($i = 1; $i <= $noStudents; $i++){
+         echo "<option value='$i'>$i</option>";
+}
+    echo "</select>";
     echo "<br>";
 }
+
+echo "<input type='submit' name='btnSubmit' value='Submit'>";   
+echo "<br>";
+
+
+$reversed = array_reverse($arrayPoints);
+var_dump($reversed);
 
 mysqli_close($conn);
 ?>
