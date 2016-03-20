25b19250a6b283e0ce0e37328f24df1451007f5f
Kabir Roshan <kabir.roshan@hotmail.co.uk>
2016-03-20 15:44:10 +0000

    MAJOR UPDATE
    New query that gets stud_fname, stud_sname, result_studpoints, studevent_result and result_position for the specific $event, $yrGroup and $gender. It also outputs the results of the query.

--- resultEntryInputForm.php
+++ resultEntryInputForm.php
@@ -24,53 +24,57 @@ $yrGroup = $_GET['cmbYrGroup'];
 $gender  = $_GET['cmbGender'];
 
 /* The query $eygQuery, where the 'eyg' stands for event, year group and gender, 
-grabs the students who meet the requirements of the specific stud_gender, 
-stud_yrgroup and event_id where a subquery is used to get the event_id. */
+grabs the stud_fname, stud_sname, result_studpoints, result_position, studevent_result 
+where the requirements of the specific stud_gender, stud_yrgroup and event_id is met */
 $eygQuery = "
-  SELECT stud_fname, stud_sname, stud_id
-  FROM students
-  WHERE stud_gender = '$gender'
-  AND stud_yrgroup = '$yrGroup'
-  AND stud_id IN (SELECT stud_id FROM result WHERE event_id = '$event')  
+    SELECT  stud_fname, 
+            stud_sname, 
+            result_studpoints, 
+            result_position, 
+            studevent_result 
+    FROM 
+        students s
+            INNER JOIN 
+        result r on 
+        s.stud_id = r.stud_id
+    WHERE 
+        stud_gender  = '$gender'
+    AND stud_yrgroup = '$yrGroup'
+    AND r.event_id   = '$event'  
 ";
-$result = mysqli_query($conn, $eygQuery);
+$result   = mysqli_query($conn, $eygQuery);
 
 //Checks to see if $result query works
 if (!$result) {
     echo "FAIL";
 }
 
-// Output
-while ($data = mysqli_fetch_assoc($result)) {
-    //echo $data['stud_fname'] . " " . $data['stud_sname'] . "<br />"; 
-    $arrayName[] = $data['stud_id']." ".$data['stud_fname'] . " " . $data['stud_sname'];
-}
-
 // A function that count the number of rows in the query $result
 $noStudents = mysqli_num_rows($result);
 
-// outputs the text boxes and drop-down lists for each name in the array.
-foreach ($arrayName as $value) {
-    echo $value;
-    $value = str_replace(' ', '', $value);
-    $value .= 'Result';
-    echo "<input type='text' name='$value' value='$value' />";
-    $value = str_replace('Result', '', $value);
-    $value .= 'Position';
-    echo "<select name='$value'>";
-       for ($i = 1; $i <= $noStudents; $i++){
-         echo "<option value='$i'>$i</option>";
-}
+// Output
+while ($data = mysqli_fetch_assoc($result)) {
+    $studName = $data['stud_fname'] . " " . $data['stud_sname'];
+    // Outputs the results of the query
+    echo $studName;
+    // Text box
+    $studName = str_replace(' ', '', $studName);
+    $studName .= 'Result';
+    echo "<input type='text' name='txtBox' value=" . $data['studevent_result'] . ">";
+    // Drop-down list
+    $studName = str_replace('Result', '', $studName);
+    $studName .= 'Position';
+    echo "<select name='cmbPosition'>";
+    echo "<option>" . $data['result_position'] . "</option>";
+    for ($i = 1; $i <= $noStudents; $i++) {
+        echo "<option value='$i'>$i</option>";
+    }
     echo "</select>";
+    // Outputs the student points that is already in the database
+    echo $data['result_studpoints'];
     echo "<br>";
 }
-
-echo "<input type='submit' name='btnSubmit' value='Submit'>";   
 echo "<br>";
-
-
-$reversed = array_reverse($arrayPoints);
-var_dump($reversed);
 
 mysqli_close($conn);
 ?>
