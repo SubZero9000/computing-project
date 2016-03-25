4a099fbd6f5553866b1328f339fa39020c81d432
Kabir Roshan <kabir.roshan@hotmail.co.uk>
2016-03-25 22:56:15 +0000

    UPDATE Query (semi) made
    Update quary made and all $_SESSION variables added.
    Need to Implement the $_SESSION variables into $updateQuery.

--- HandleForm.php
+++ HandleForm.php
@@ -18,26 +18,41 @@ if (!$conn) {
 echo "<br>";
 
 session_start();
-
 /* loops through each row in the global $_SESSION variable which
 contains the array and uses the $value to GET the data in the text
 boxes and output them */
 
+// studevent_result = 
 foreach ($_SESSION['arrayNameResult'] as $value) {
 $studResult = $_GET[$value];
 echo $studResult;
 echo "<br>";
 }
 
+// result_postion = 
 foreach ($_SESSION['arrayNamePosition'] as $value) {
 $studPosition = $_GET[$value];
 echo $studPosition;
 echo "<br>";
 }
 
-$insertQuery = "
-    INSERT INTO result (studevent_result, result_position)
-";
+echo "<br>";
 
+// stud_id = 
+foreach ($_SESSION['arrayId'] as $value) {
+echo $value;
+echo "<br>";
+}
+
+// UPDATE query, this will update the studevent_result and result_position
+// column in the database for the specific stud_id.
+$updateQuery = "
+    UPDATE result
+    SET studevent_result = '00:20:33',
+        result_position = '6'
+    WHERE result.stud_id = '12'
+";
+$updateRow = mysqli_query($conn, $updateQuery);
+mysqli_close($conn);
 ?>
 </html>
